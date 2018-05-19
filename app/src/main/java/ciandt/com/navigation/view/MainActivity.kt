41f84dfd49f663acbe9bb9a2b8a1ef7a8702f6a4
Alan Alencar da Silva <alans@ciandt.com>
2018-05-19 11:46:25 -0300

    [alans]  mustard requirement for ble

--- app/src/main/java/ciandt/com/navigation/view/MainActivity.kt
+++ app/src/main/java/ciandt/com/navigation/view/MainActivity.kt
@@ -11,6 +11,8 @@ import ciandt.com.navigation.BuildConfig
 import ciandt.com.navigation.R
 import ciandt.com.navigation.model.NotificationCreator
 import com.estimote.coresdk.common.config.EstimoteSDK
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.Requirement
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.RequirementsWizardFactory
 import com.estimote.proximity_sdk.proximity.EstimoteCloudCredentials
 import com.estimote.proximity_sdk.proximity.ProximityAttachment
 import com.estimote.proximity_sdk.proximity.ProximityObserver
@@ -37,6 +39,11 @@ class MainActivity : AppCompatActivity() {
     private var proximityObservationHandler: ProximityObserver.Handler? = null
     private val cloudCredentials = EstimoteCloudCredentials(BuildConfig.ESTIMOTE_APP_ID_DEBUG, BuildConfig.ESTIMOTE_APP_TOKEN_DEBUG)
     // Estimote proximity end
+
+    // Estimote Toast ini
+    private val displayToastAboutMissingRequirements: (List<Requirement>) -> Unit = { Toast.makeText(this, "Unable to start proximity observation. Requirements not fulfilled: ${it.size}", Toast.LENGTH_SHORT).show() }
+    private val displayToastAboutError: (Throwable) -> Unit = { Toast.makeText(this, "Error while trying to start proximity observation: ${it.message}", Toast.LENGTH_SHORT).show() }
+    // Estimote Toast end
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -75,24 +82,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         // Estimote ini
-        EstimoteSDK.initialize(applicationContext, BuildConfig.ESTIMOTE_APP_ID_DEBUG, BuildConfig.ESTIMOTE_APP_TOKEN_DEBUG)
+        EstimoteSDK.initialize(applicationContext,
+                BuildConfig.ESTIMOTE_APP_ID_DEBUG,
+                BuildConfig.ESTIMOTE_APP_TOKEN_DEBUG)
         // Estimote end
 
         // Estimote proximity ini
         notification = NotificationCreator().createNotification(this)
+        // Estimote proximity end
 
+        // Estimote requirement wizard ini
+        RequirementsWizardFactory.createEstimoteRequirementsWizard().fulfillRequirements(
+                this,
+                onRequirementsFulfilled = { startProximityObservation() },
+                onRequirementsMissing = displayToastAboutMissingRequirements,
+                onError = displayToastAboutError
+        )
+        // Estimote requirement wizard end
+
+        setContentView(R.layout.activity_main)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+    }
+
+    // Estimote proximity
+    override fun onDestroy() {
+        super.onDestroy()
+        proximityObservationHandler?.stop()
+    }
+
+    // Estimote proximity
+    private fun startProximityObservation() {
         proximityObserver =
                 ProximityObserverBuilder(applicationContext, cloudCredentials)
                         .withScannerInForegroundService(notification)
                         .withBalancedPowerMode()
                         .build()
-        // Estimote proximity end
-
-        setContentView(R.layout.activity_main)
-
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-
         // Estimote zones ini
+
         // Recepção
         val receptionRoomZone = proximityObserver.zoneBuilder()
                 .forAttachmentKeyAndValue(AKEY_VENUE, AVALUE_RECEPTION)
@@ -118,16 +145,12 @@ class MainActivity : AppCompatActivity() {
                 .create()
         // Estimote zones end
 
+        // Estimote observer ini
         proximityObservationHandler = proximityObserver
                 // Adding the Estimote zones...
                 .addProximityZones(receptionRoomZone, mallZone, garageZone)
                 .start()
-    }
-
-    // Estimote proximity
-    override fun onDestroy() {
-        super.onDestroy()
-        proximityObservationHandler?.stop()
+        // Estimote observer end
     }
 
     // Estimote proximity
